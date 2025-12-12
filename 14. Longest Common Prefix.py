9acf44bf072d351184a5a25dc6560f1efdd62484
Md Nazrul Islam <arfanislamabir@gmail.com>
2025-12-13 01:35:04 +0600

    check

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,25 @@
+
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+    
+    prefix = strs[0]
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(longestCommonPrefix(["flower","flow","flight"]))
